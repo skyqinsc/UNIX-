c5e2f5d15d7e1e47756ad1eac13bb8cbb1088ca0
skyqinsc <skyqinsc@gmail.com>
2015-10-29 10:55:56 +0800

    first commit

--- byteOrder.cpp
+++ byteOrder.cpp
@@ -4,20 +4,20 @@ using namespace std;
 
 int main()
 {
-	union {
+    union {
         short s;
         char c[sizeof(short)];
-	}un;
-	un.s=0x0102;
-	//printf("%d==\n",un.s);
-	printf("%c %c\n",un.c[0],un.c[1]);
-	if(sizeof(short)==2){
-		if(un.c[0]==1&&un.c[1]==2)
-			printf("little-endian\n");
-		else
-			printf("big-endian\n");
-	}
-	else
-		printf("sizeof(short) = %d\n",sizeof(short));
-	return 0;
+    }un;
+    un.s=0x0102;
+    //printf("%d==\n",un.s);
+    printf("%c %c\n",un.c[0],un.c[1]);
+    if(sizeof(short)==2){
+        if(un.c[0]==1&&un.c[1]==2)
+            printf("little-endian\n");
+        else
+            printf("big-endian\n");
+    }
+    else
+        printf("sizeof(short) = %d\n",sizeof(short));
+    return 0;
 }
